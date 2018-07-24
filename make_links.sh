cd4c4b22fec8ae872aa69020fda0bb126f118d26
Mårten Nilsson <m@martennilsson.se>
2018-07-24 13:42:10 +0200

    Added script to link dotfiles

---
+++ make_links.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+ln -sf "$PWD/xinitrc" ~/.xinitrc
+ln -sf "$PWD/vimrc" ~/.vimrc
+ln -sf "$PWD/zshrc" ~/.zshrc
+ln -sf "$PWD/zprofile" ~/.zprofile
+ln -sf "$PWD/i3config" ~/.config/i3/config
+ln -sf "$PWD/i3statusconfig" ~/.config/i3status/config
+ln -sf "$PWD/xfce4-terminalrc" ~/.config/xfce4/terminal/terminalrc
+
